5ab6bc3e36d13ac59371dd05bcd53f0fbf0469b5
Tomiwa Ademidun <tomiademidun@gmail.com>
2017-11-13 03:37:39 -0500

    Refactored create blog post

--- src/app/blog-post-create/blog-post-create.component.ts
+++ src/app/blog-post-create/blog-post-create.component.ts
@@ -5,6 +5,9 @@ import 'tinymce/themes/modern';
 
 import 'tinymce/plugins/table';
 import 'tinymce/plugins/link';
+import 'tinymce/plugins/toc';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/lists';
 
 declare var tinymce: any;
 
@@ -33,7 +36,7 @@ export class BlogPostCreateComponent implements OnInit, AfterViewInit, OnDestroy
   ngAfterViewInit() {
     tinymce.init({
       selector: '#' + this.elementId,
-      plugins: ['link', 'table'],
+      plugins: ['link', 'table','toc','preview','lists'],
       skin_url: 'assets/skins/lightgray',
       setup: editor => {
         this.editor = editor;
@@ -63,5 +66,12 @@ export class BlogPostCreateComponent implements OnInit, AfterViewInit, OnDestroy
     }
 
   }
+  /** TODO: preload created blog posts
+   * <h3 id="mcetoc_1buq82v580"><a href="http://blog.tomiwa.ca/hedged-optimism/">Hedged Optimism</a></h3> 
+   * <p>This was potentially my favorite blog post to write.</p> <ol> <li>It was actionable</li> <li>It was on a 
+   * topic which I really enjoyed.</li> <li>I think I may be right.&nbsp;</li> </ol> <blockquote> <p>"An investment in 
+   * education pays the best interest."</p> <p>&nbsp; &nbsp; - Benjamin Franklin</p> </blockquote> <p>This is why I
+   *  would prefer going back to my regular writing.</p> <p>&nbsp;</p>
+   */
 
 }
