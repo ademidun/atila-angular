1cbaf830d03f081f77e47922a6c67be4007b2a0e
Tomiwa Ademidun <tomiademidun@gmail.com>
2018-02-16 10:16:22 -0500

    fixed webpack issues, refactored scholarships list display

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,9 @@ deploy () {
  git push ;
  # https://github.com/angular/angular-cli/issues/5618#issuecomment-348225508
  npm run ng-high-mem -- build --prod;
- firebase deploy;
+ # firebase deploy;
  # ng build --prod ;
 # node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --target=production;
 # ng build --prod ; firebase deploy;
- # firebase deploy —-only hosting ;
    return 0
 }
