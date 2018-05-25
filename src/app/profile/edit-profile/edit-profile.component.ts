88192b7a5ea964c6aee00f3c8fce47e2436a5d46
ademidun <tomiademidun@gmail.com>
2018-05-24 21:06:43 -0400

    refactored edit profile component

--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,33 +1,41 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { UserProfile, toTitleCase } from '../../_models/user-profile';
-import { UserProfileService } from "../../_services/user-profile.service";
+import {toTitleCase, UserProfile} from '../../_models/user-profile';
+import {UserProfileService} from "../../_services/user-profile.service";
 
-import {NgForm, NgModel} from '@angular/forms';
+import {FormControl, NgModel} from '@angular/forms';
 
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 
-import { Router, ActivatedRoute } from '@angular/router'
-import {MatAutocompleteSelectedEvent, MatSnackBar} from '@angular/material';
+import {ActivatedRoute, Router} from '@angular/router'
+import {MatSnackBar} from '@angular/material';
 
-import { Title }     from '@angular/platform-browser';
+import {Title} from '@angular/platform-browser';
 
-import { UploadFile } from '../../_models/upload-file';
+import {UploadFile} from '../../_models/upload-file';
 
-import { AuthService } from "../../_services/auth.service";
+import {AuthService} from "../../_services/auth.service";
 
-import { MyFirebaseService } from "../../_services/myfirebase.service";
+import {MyFirebaseService} from "../../_services/myfirebase.service";
 
 import * as firebase from "firebase";
 import {environment} from '../../../environments/environment';
-
-import {FormControl} from '@angular/forms';
 import {startWith} from 'rxjs/operators/startWith';
 import {map} from 'rxjs/operators/map';
-import {debounceTime} from 'rxjs/operators/debounceTime';
-import {ACTIVITIES, COUNTRIES, DISABILITY, ETHNICITY, RELIGION, SCHOOLS_DICT, SPORTS} from '../../_models/constants';
+import {
+  ACTIVITIES,
+  COUNTRIES,
+  DISABILITY,
+  ETHNICITY,
+  LANGUAGE,
+  MAJORS_LIST,
+  RELIGION,
+  SCHOOLS_DICT,
+  SCHOOLS_LIST,
+  SPORTS
+} from '../../_models/constants';
 import {ScholarshipService} from '../../_services/scholarship.service';
-import {SCHOOLS_LIST, MAJORS_LIST, LANGUAGE} from '../../_models/constants';
 import {prettifyKeys} from '../../_shared/utils';
+
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -43,7 +51,7 @@ export class EditProfileComponent implements OnInit {
     'Workplace or Apprenticeship',
   ];
 
-EDUCATION_FIELD = [
+  EDUCATION_FIELD = [
     'Arts (Undergrad)',
     'STEM (Undergrad)',
     'Trade School',
@@ -54,11 +62,11 @@ EDUCATION_FIELD = [
     'Arts (Grad School)',
     'STEM (Grad School)',
     'Other'
-];
+  ];
 
   userProfile = new UserProfile();
   profileInfo = true;
-  documentScholarshipsPercent =87;
+  documentScholarshipsPercent = 87;
 
   Object = Object;
   userName: string;
@@ -101,17 +109,14 @@ EDUCATION_FIELD = [
     'language': LANGUAGE,
   };
 
-
-  constructor(
-    public userProfileService: UserProfileService,
-    public router: Router,
-    public route: ActivatedRoute,
-    public snackBar: MatSnackBar,
-    public titleService: Title,
-    public authService: AuthService,
-    public scholarshipService: ScholarshipService,
-    public firebaseService: MyFirebaseService,
-  ) {
+  constructor(public userProfileService: UserProfileService,
+              public router: Router,
+              public route: ActivatedRoute,
+              public snackBar: MatSnackBar,
+              public titleService: Title,
+              public authService: AuthService,
+              public scholarshipService: ScholarshipService,
+              public firebaseService: MyFirebaseService,) {
     this.userName = route.snapshot.params['username'];
 
     this.schoolNames = SCHOOLS_DICT.map(school => school.name);
@@ -125,13 +130,13 @@ EDUCATION_FIELD = [
           data => {
             this.userProfile = data;
 
-            let profileTitle = this.userProfile.first_name +' '+ this.userProfile.last_name + "'s Profile";
+            let profileTitle = this.userProfile.first_name + ' ' + this.userProfile.last_name + "'s Profile";
             this.titleService.setTitle('Atila - ' + profileTitle);
             this.initializeLocations(this.userProfile.city);
 
             this.initializeForm();
-            console.log('this.route',this.route,this.route.url);
-            if (this.route.snapshot.fragment && this.route.snapshot.fragment.includes('referral-code')){
+            console.log('this.route', this.route, this.route.url);
+            if (this.route.snapshot.fragment && this.route.snapshot.fragment.includes('referral-code')) {
               this.showReferralCode = true;
             }
 
@@ -182,24 +187,26 @@ EDUCATION_FIELD = [
   }
 
 
-  switchPage(){
+  switchPage() {
     this.profileInfo = !this.profileInfo;
   }
+
 // https://stackoverflow.com/questions/44227682/angular2-dynamically-add-inputs-for-an-array-of-items
 
-initializeLocations(cities: Array<any>){
+  initializeLocations(cities: Array<any>) {
 
-    if(cities.length>0){
-      this.locationData.city= cities[0].name;
-      this.locationData.country=cities[0].country;
-      this.locationData.province=cities[0].province;
+    if (cities.length > 0) {
+      this.locationData.city = cities[0].name;
+      this.locationData.country = cities[0].country;
+      this.locationData.province = cities[0].province;
     }
 
   }
+
   saveProfile(profileForm?) {
 
-  this.userProfile.metadata['stale_cache'] = true;
-    if(!profileForm) {
+    this.userProfile.metadata['stale_cache'] = true;
+    if (!profileForm) {
 
 
       let sendData = {
@@ -212,11 +219,11 @@ initializeLocations(cities: Array<any>){
         .subscribe(
           data => {
 
-            this.showSnackBar("Successfully Updated Your Profile.",'', 3000);
+            this.showSnackBar("Successfully Updated Your Profile.", '', 3000);
           },
           err => {
 
-            this.showSnackBar('Profile updated unsuccessfully - ' + err.error? err.error: err,'', 3000);
+            this.showSnackBar('Profile updated unsuccessfully - ' + err.error ? err.error : err, '', 3000);
           }
         );
       return;
@@ -231,30 +238,29 @@ initializeLocations(cities: Array<any>){
         .subscribe(
           data => {
 
-            this.showSnackBar("Successfully Updated Your Profile.",'', 3000);
+            this.showSnackBar("Successfully Updated Your Profile.", '', 3000);
           },
           err => {
 
-            this.showSnackBar('Profile updated unsuccessfully - ' + err.error? err.error: err,'', 3000);
+            this.showSnackBar('Profile updated unsuccessfully - ' + err.error ? err.error : err, '', 3000);
           }
         )
 
 
-
     } else {
-      this.showSnackBar("Form is not valid",'', 3000);
+      this.showSnackBar("Form is not valid", '', 3000);
     }
   }
 
 
-  fileChangeEvent(fileInput: any){
+  fileChangeEvent(fileInput: any) {
 
     //TODO: this seems a bit redundant
     this.formFile = fileInput.target.files[0];
     this.formFileEvent = fileInput;
   }
 
-  uploadUserDocuments(){
+  uploadUserDocuments() {
     //let uploadOperation: Observable<any>;
 
     //create Upload file and configure its properties before uploading.
@@ -267,9 +273,9 @@ initializeLocations(cities: Array<any>){
     this.uploadFile.name = this.authService.hashFileName(this.formFile.name);
     // this.uploadFile.path = "user-profiles/" + this.userProfile.user + "/documents/";
     this.uploadFile.path = "user-profiles/" + this.userProfile.user + "/documents/";
-    if(this.userProfile.metadata['test_mode']) {
+    if (this.userProfile.metadata['test_mode']) {
 
-      this.uploadFile.path = "user-profiles/" + 777+ "/documents/";
+      this.uploadFile.path = "user-profiles/" + 777 + "/documents/";
     }
     this.uploadFile.path = this.uploadFile.path + this.uploadFile.name;
 
@@ -285,7 +291,7 @@ initializeLocations(cities: Array<any>){
             },
             (error) => {
 
-              this.snackBar.open('Upload Error','',{ duration: 3000});
+              this.snackBar.open('Upload Error', '', {duration: 3000});
               this.uploadProgress = null;
             },
             () => {
@@ -304,31 +310,31 @@ initializeLocations(cities: Array<any>){
             });
         },
         err => {
-          this.showSnackBar(err.message,'', 3000);
+          this.showSnackBar(err.message, '', 3000);
         },);
 
   }
 
   deleteArrayitem(arr: any[], index) {
-    arr.splice(index,1)
+    arr.splice(index, 1)
   }
 
 
-
 // SnackBar notification
-showSnackBar(text: string, action = '', duration: number) {
-  this.snackBar.open(text, action, {
-    duration: duration
-  });
-}
+  showSnackBar(text: string, action = '', duration: number) {
+    this.snackBar.open(text, action, {
+      duration: duration
+    });
+  }
 
-  verifyAccount(){
+  verifyAccount() {
     this.userProfileService.refreshVerificationToken(this.userProfile.username)
       .subscribe(
-        res =>  {
+        res => {
           this.verificationResponse = res.message;
         },
-              err => {},
+        err => {
+        },
       )
   }
 
@@ -344,10 +350,10 @@ showSnackBar(text: string, action = '', duration: number) {
    * https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
    * https://stackoverflow.com/questions/42341930/google-places-autocomplete-angular2
    */
-  placeAutoComplete(placeResult:any, locationModel: NgModel, options= {}){ //Assign types to the parameters place result is a PlaceResult Type, see documentation
+  placeAutoComplete(placeResult: any, locationModel: NgModel, options = {}) { //Assign types to the parameters place result is a PlaceResult Type, see documentation
 
 
-    this.predictLocation(this.locationData, placeResult,options);
+    this.predictLocation(this.locationData, placeResult, options);
 
   }
 
@@ -356,11 +362,11 @@ showSnackBar(text: string, action = '', duration: number) {
    * @param location
    * @param placeResult
    */
-  predictLocation(location, placeResult, options= {}){
+  predictLocation(location, placeResult, options = {}) {
 
     options['object_key'] = options['object_key'] || 'locationData';
 
-    var addressComponents = placeResult.address_components ;
+    var addressComponents = placeResult.address_components;
 
     var keys = ['city', 'province', 'country'];
 
@@ -368,15 +374,15 @@ showSnackBar(text: string, action = '', duration: number) {
 
 
     addressComponents.forEach((element, i, arr) => {
-      if(element.types[0]=='locality' || element.types[0]=='administrative_area_level_3' ||  element.types[0]=='postal_town'||  element.types[0]=='sublocality_level_1'){
+      if (element.types[0] == 'locality' || element.types[0] == 'administrative_area_level_3' || element.types[0] == 'postal_town' || element.types[0] == 'sublocality_level_1') {
         this[options['object_key']].city = element.long_name;
       }
 
-      if(element.types[0]=='administrative_area_level_1'){
+      if (element.types[0] == 'administrative_area_level_1') {
         this[options['object_key']].province = element.long_name;
       }
 
-      if(element.types[0]=='country'){
+      if (element.types[0] == 'country') {
         this[options['object_key']][element.types[0]] = element.long_name;
       }
     });
@@ -394,7 +400,7 @@ showSnackBar(text: string, action = '', duration: number) {
    */
   keyDownHandler(event: Event) {
 
-    if((<KeyboardEvent>event).keyCode == 13) {
+    if ((<KeyboardEvent>event).keyCode == 13) {
 
       event.preventDefault();
     }
@@ -404,13 +410,13 @@ showSnackBar(text: string, action = '', duration: number) {
   /**
    * If the Google Place API did not load, then change the placeholder message to only ask for a city (or country?).
    */
-  googlePlaceNoLoad(){
+  googlePlaceNoLoad() {
     // TODO: Figure out if we need to include this function googlePlaceNoLoad() for the googlePlaceDirective to work.
     this.locationPlaceHolder = 'City'
   }
 
   typeaheadEvent(event) {
-      this.userProfile[event.type] = event.event.item;
+    this.userProfile[event.type] = event.event.item;
   }
 
   prettifyKeys(str) {
