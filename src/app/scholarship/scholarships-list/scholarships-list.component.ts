ab14ca780dc793e1ab03dd6a0682b0dea4a7c32d
ademidun <tomiademidun@gmail.com>
2018-09-25 22:25:49 -0400

    load scholarship preview form with just url query or no preview form data

--- src/app/scholarship/scholarships-list/scholarships-list.component.ts
+++ src/app/scholarship/scholarships-list/scholarships-list.component.ts
@@ -135,9 +135,22 @@ export class ScholarshipsListComponent implements OnInit {
       this.scholarshipService.getScholarshipPreviewForm()
       .then(
         res => {
+          console.log('this.activatedRoute.snapshot.queryParams[\'q\']',this.activatedRoute.snapshot.queryParams['q']);
           this.form_data = res;
           if(this.form_data){
             this.form_data['filter_by_user_show_eligible_only']=true;
+          }
+          else {
+            if (this.activatedRoute.snapshot.queryParams['q']) {
+              this.form_data = {
+                previewMode: 'universalSearch',
+                searchString: this.activatedRoute.snapshot.queryParams['q'],
+                previewFormFromUrl: true,
+              }
+            }
+            else {
+              this.form_data = {featuredScholarshipsMode: true};
+            }
           }
         },
 
