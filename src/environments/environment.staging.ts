9d92f1c58b4dd6ea965b016c8b22403ed113bc2a
Tomiwa Ademidun <tomiademidun@gmail.com>
2018-12-15 17:05:00 -0500

    fixed environment api url to use correct staging apiUrl

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ import {environment as prodEnvironment} from './environment.prod';
 
 const stagingEnvironment =  prodEnvironment;
 
-// stagingEnvironment.apiUrl = 'https://atila-7-staging.herokuapp.com/api/';
+stagingEnvironment.apiUrl = 'https://atila-7-staging.herokuapp.com/api/';
 stagingEnvironment.name = 'staging';
 export const environment = stagingEnvironment;
