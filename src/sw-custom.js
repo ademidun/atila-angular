9a273b6e1015ba0714dc49dbb82cf05083a71ac7
Tomiwa Ademidun <tomiademidun@gmail.com>
2019-03-30 19:07:16 -0400

    added custom service workers scripts and new favicon

---
+++ src/sw-custom.js
@@ -0,0 +1,11 @@
+// sw-custom.js – a file containing a event listener
+// http://jakubcodes.pl/2018/06/13/enhancing-angular-ngsw/
+
+(function () {
+  'use strict';
+  self.addEventListener('notificationClick', (event) => {
+    event.notification.close();
+    console.log('notification details: ', event.notification);
+    console.log('notification event: ', {event});
+  });
+}());
---
+++ src/sw-master.js
@@ -0,0 +1,5 @@
+// sw-master.js – a file combining ngsw with our custom logic
+// http://jakubcodes.pl/2018/06/13/enhancing-angular-ngsw/
+
+importScripts('./ngsw-worker.js');
+importScripts('./sw-custom.js');
