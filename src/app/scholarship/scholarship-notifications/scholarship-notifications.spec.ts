b67f5f99c6e592cd5d88584b4b5476516709a5d7
Tomiwa Ademidun <tomiademidun@gmail.com>
2019-06-08 08:51:01 -0400

    created a test for ScholarshipNotificationsxx

---
+++ src/app/scholarship/scholarship-notifications/scholarship-notifications.spec.ts
@@ -0,0 +1,24 @@
+import {createTestUserProfile} from '../../_models/user-profile';
+import {createTestScholarship} from '../../_models/scholarship';
+import {notifySavedScholarship} from './scholarship-notifications'
+import {userProfileServiceStub} from '../../_services/user-profile.service';
+import {NotificationsServiceStub} from '../../_services/notifications.service';
+import {TestBed} from '@angular/core/testing';
+import {NotificationDialogComponent} from '../../notification-dialog/notification-dialog.component';
+
+describe('notifySavedScholarship', function() {
+  it('#notifySavedScholarship(): Should make sure not sent if user is not admin or testing', function() {
+
+
+    const userProfile = createTestUserProfile();
+    userProfile.is_debug_mode = false;
+    userProfile.is_atila_admin = false;
+    const scholarship = createTestScholarship();
+    const dialogComponent = TestBed.createComponent(NotificationDialogComponent);
+
+    const result = notifySavedScholarship(scholarship, userProfile, userProfileServiceStub, NotificationsServiceStub, dialogComponent);
+
+    expect(result).toBeFalsy('notifySavedScholarship Result should be false for non admin or test_mode user');
+
+  });
+});
