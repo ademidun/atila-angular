fb1dbe5f3e827da8aeb09c48a9ace94020c81a14
Tomiwa Ademidun <tomiademidun@gmail.com>
2019-07-06 08:20:12 -0400

    fixed environment name for staging atilaMicroservicesNodeApiUrl

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,6 +3,6 @@ import {environment as prodEnvironment} from './environment.prod';
 export const stagingEnvironment =  Object.assign({}, prodEnvironment);
 
 stagingEnvironment.apiUrl = 'https://atila-7-staging.herokuapp.com/api/';
-stagingEnvironment.atilaMicroservicesNodeApiUrl = 'https://tgrr8bis30.execute-api.us-east-1.amazonaws.com/prod/';
+stagingEnvironment.atilaMicroservicesNodeApiUrl = 'https://tgrr8bis30.execute-api.us-east-1.amazonaws.com/staging/';
 stagingEnvironment.name = 'staging';
 export const environment = stagingEnvironment;
