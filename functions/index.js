ed41b3358867e5f98f202c33dd0954ddc482dd9f
Tomiwa Ademidun <tomiademidun@gmail.com>
2019-08-10 13:26:05 -0400

    modified index.js to use demo rendertron url and removed unused comments

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,8 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-// const admin = require('firebase-admin');
-// admin.initializeApp();
-const nodemailer = require('nodemailer');
 const {sendContactEmail} = require('./email');
 const {fcmSend} = require('./pushNotifications');
 
-
-// exports.sendContactEmail = functions.database.ref('/contact_form/{pushId}/')
-//   .onCreate((snapshot, context) => {
-//     const gmailEmail = process.env.gmail.email;
-//     const gmailPassword = process.env.gmail.password;
-//     const mailTransport = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: gmailEmail,
-//         pass: gmailPassword,
-//       },
-//     });
-//
-//     const APP_NAME = 'Atila';
-//
-//     // Grab the current value of what was written to the Realtime Database.
-//     const contactData = snapshot.val();
-//     console.log('contactData', context.params.pushId, contactData);
-//
-//     let mailOptions = {
-//       from: `${APP_NAME} <tomiademidun@gmail.com>`,
-//       to: 'info@atila.ca',
-//     };
-//
-//     mailOptions.subject = `Atila Contact Form to ${APP_NAME}!`;
-//     mailOptions.text = `New Contact from Atila. \n
-//     Email: ${contactData.email} \n
-//     Name: ${contactData.first_name + '' + contactData.last_name} \n
-//     Message: ${contactData.message}\n
-//     Data:\n`;
-//     console.log('mailOptions', mailOptions);
-//
-//     mailOptions.text += String(contactData);
-//     return sendEmail(email, emailText);
-//
-//   });
-//
-// function sendEmail(email, mailOptions) {
-//   // The user subscribed to the newsletter.
-//
-//   return mailTransport.sendMail(mailOptions).then(() => {
-//     return console.log('Contact email sent to:', email);
-//   });
-// }
 
 const express = require('express');
 const fetch = require('node-fetch');
@@ -58,11 +10,11 @@ const url = require('url');
 const app = express();
 
 
-// You might instead set these as environment varibles
+// You might instead set these as environment variables
 // I just want to make this example explicitly clear
 const appUrl = 'atila.ca';
-// const renderUrl = 'https://render-tron.appspot.com/render';
-const renderUrl = 'https://atila-7.appspot.com/render';
+const renderUrl = 'https://render-tron.appspot.com/render';
+// const renderUrl = 'https://atila-7.appspot.com/render';
 
 
 // Generates the URL
@@ -104,12 +56,15 @@ function detectBot(userAgent) {
 
   for (const bot of bots) {
     if (agent.indexOf(bot) > -1) {
-      console.log('bot detected', bot, agent)
+      console.log('bot detected', bot, agent);
+      console.log({ renderUrl });
+
       return true
     }
   }
 
   console.log('no bots found');
+  console.log({ renderUrl });
   return false
 
 }
